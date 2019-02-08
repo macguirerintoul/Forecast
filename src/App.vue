67a00cb1705e09f3d9640c26369ed897f968a2cd
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-02-08 11:52:11 -0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Event v-for="event in events" v-bind:title="event.title" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Event from './components/Event.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Event
+  },
+  data() {
+    return {
+      events: [
+        {
+          title: "event 1"
+        },
+        {title: "event 2"},
+        {title: "event 3"},
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/Event.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="event">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Event',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
